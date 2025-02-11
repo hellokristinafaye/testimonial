590d7fefc01cefba135695b1ff43ac01a9dcd2c4
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-10 20:03:51 -0800

    onClick WORKS to remove class

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("not like us");
+
+let userTexts = document.getElementsByClassName('user-text');
+let userPics = document.getElementsByClassName('user-pic');
+
+function showReview() {
+    for (userPic of userPics) {
+        userPic.classList.remove('active-pic');
+    }
+    for (userText of userTexts) { 
+        userText.classList.remove('active-text');
+    }
+}

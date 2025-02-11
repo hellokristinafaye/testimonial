4586531cc8c10fdfe91729fbbecb53c15180c5cc
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-10 20:12:28 -0800

    showReview works!

--- script.js
+++ script.js
@@ -10,4 +10,9 @@ function showReview() {
     for (userText of userTexts) { 
         userText.classList.remove('active-text');
     }
+    let i = Array.from(userPics).indexOf(event.target);
+
+    userPics[i].classList.add('active-pic');
+    userTexts[i].classList.add('active-text');
+    // great it works! had the wrong class for text, it said pic instead. So it wasn't event at all.  So thanks Reddit?  they were actually right lol
 }
